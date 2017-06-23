6895b546719a2a0cc00907bb50caf105c552bbcc
GoJek <ug201310029@iitj.ac.in>
2017-06-23 12:21:22 +0530

    Javascript styling added in gameoflife.js

--- src/game_of_life.js
+++ src/game_of_life.js
@@ -1,6 +1,6 @@
-
-var row = prompt("Enter row");
-var col = prompt("Enter column");
+'use strict'
+let row = prompt("Enter row");
+let col = prompt("Enter column");
 
 document.getElementById('myCanvas').setAttribute("width", ""+row+"");
 document.getElementById('myCanvas').setAttribute("height", ""+col+"");
@@ -17,12 +17,12 @@ function main() {
 
 
 function show_grid() {
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
+  let c = document.getElementById("myCanvas");
+  let ctx = c.getContext("2d");
   ctx.clearRect(0,0,row,col);
   for(key in alive){
     ctx.fillStyle = '#FF1005';
-    ctx.fillRect((alive[key].x)*3,(alive[key].y)*3,3,3);
+    ctx.fillRect( (alive[key].x)*3, (alive[key].y)*3,3,3 );
   }
 }
 
